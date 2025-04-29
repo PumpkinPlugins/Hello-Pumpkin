5e2024e65d1728df8377b26bb10ad7fe5a31d27f
vyPal <kubik.palacky@gmail.com>
2025-04-29 16:45:28 +0200

    Update to support latest API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use pumpkin::plugin::{player::player_join::PlayerJoinEvent, Context, EventHandler, EventPriority};
+use pumpkin::{
+    plugin::{player::player_join::PlayerJoinEvent, Context, EventHandler, EventPriority},
+    server::Server,
+};
 use pumpkin_api_macros::{plugin_impl, plugin_method, with_runtime};
 use pumpkin_util::text::{color::NamedColor, TextComponent};
 
@@ -10,7 +13,7 @@ struct MyJoinHandler;
 #[with_runtime(global)]
 #[async_trait]
 impl EventHandler<PlayerJoinEvent> for MyJoinHandler {
-    async fn handle_blocking(&self, event: &mut PlayerJoinEvent) {
+    async fn handle_blocking(&self, _server: &Arc<Server>, event: &mut PlayerJoinEvent) {
         event.join_message =
             TextComponent::text(format!("Welcome, {}!", event.player.gameprofile.name))
                 .color_named(NamedColor::Green);
